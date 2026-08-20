b6b268196983c538a8d7f20be4e55ffb63bbc5ba
Luizcds.dev <luizcds.dev@gmail.com>
2026-08-20 10:13:59 -0300

    commit codigo simples de produtos

---
+++ Gerais/Produto_Valores.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+int val1, val2, produto;
+
+printf("Digite um valor\n");
+scanf("%d",&val1);
+
+
+printf("Digite um valor\n");
+scanf("%d",&val2);
+
+
+produto = (val1*val2);
+
+
+printf("\n O produto dos dois valores e: %d\n", produto);
+
+
+
+
+}
